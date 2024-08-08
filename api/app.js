75c23d38c2e6ec60df2b18f7335dff72c380931d
Nimeth Fernando <108478292+nimeth42@users.noreply.github.com>
2024-08-08 10:35:25 +0530

    connections

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
-import postRoute from "./routes/post.route";
+import authRoute from "./routes/post.route.js";
+import postRoute from "./routes/post.route.js";
 
 
 const app = express();
 
 app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
     console.log("Server is runniing!"); 'Server is running!'
