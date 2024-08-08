0e6e664c51da0956b857d0aa44fe25a3c48d6405
Nimeth Fernando <108478292+nimeth42@users.noreply.github.com>
2024-08-08 08:54:28 +0530

    router.post

--- api/app.js
+++ api/app.js
@@ -1,25 +1,10 @@
 import express from "express";
+import postRoute from "./routes/post.route";
+
 
 const app = express();
 
-app.use("/api/test", (req,res)=>{
-    res.send("It works!")
-})
-app.use("/api/auth/register", (req,res)=>{
-    res.send("It works")
-})
-
-app.use("/api/auth/login", (req,res)=>{
-    res.send("It works")
-})
-
-app.use("/api/auth/logout", (req,res)=>{
-    res.send("It works")
-})
-
-app.use("/api/posts/", (req,res)=>{
-    res.send("It works")
-})
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
     console.log("Server is runniing!"); 'Server is running!'
