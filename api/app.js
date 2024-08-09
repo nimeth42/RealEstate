efa50c77d054aad256844b3b3be7ac0acd5f7cc6
Nimeth Fernando <108478292+nimeth42@users.noreply.github.com>
2024-08-09 08:59:57 +0530

    nodemon

--- api/app.js
+++ api/app.js
@@ -9,5 +9,5 @@ app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
-    console.log("Server is runniing!"); 'Server is running!'
+    console.log("Server is runniing!"); 
 });
