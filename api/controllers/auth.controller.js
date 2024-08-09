9ac5dd2d225c83ea3a74555938d1bb10ab5292f6
Nimeth Fernando <108478292+nimeth42@users.noreply.github.com>
2024-08-09 16:45:45 +0530

    bcypt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,17 @@
-export const register = (req, res) => {
+import bcrypt from "bcrypt";
+export const register = async (req, res) => {
+    const {username,email, password } = req.body;
     //db operations
-    console.log(req.body)
-}
+    
+    //HASH THE password
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword)
+    //CREATE A NEW USER AND SAVE TO DB
+};
 export const login = (req, res)=>{
     //db operations
-}
+};
 export const logout = (req, res)=>{
     //db operations
-}
+};
