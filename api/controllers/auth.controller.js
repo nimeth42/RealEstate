6bf05d44da301d7233f549d9a5fd022a7e2eb6d4
Nimeth Fernando <108478292+nimeth42@users.noreply.github.com>
2024-08-23 07:49:55 +0530

    db update

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import prisma from "../lib/prisma.js";
 import bcrypt from "bcrypt";
 export const register = async (req, res) => {
     const {username,email, password } = req.body;
@@ -8,7 +9,15 @@ export const register = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     console.log(hashedPassword)
     //CREATE A NEW USER AND SAVE TO DB
-    const newUser = await 
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    console.log(newUser)
 };
 export const login = (req, res)=>{
     //db operations
