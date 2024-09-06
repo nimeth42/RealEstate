0dc5376a3e0d59de869401d9d69a2db6e31f11ec
Nimeth Fernando <108478292+nimeth42@users.noreply.github.com>
2024-09-06 15:47:04 +0530

    verifications

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -10,9 +10,23 @@ export const shouldBeLoggedIn = async (req,res)=>{
         if(err)res.status(403).json({message: "Token is not Valid!"});
     })
 
-};
+    res.status(200).json({message: "You are Authenticated"});
+
+};  
 
 export const shouldBeAdmin = async (req,res)=>{
+    const token = req.cookies.token;
+
+    if(!token) return res.status(401).json({message: "Not Authenticated!"});
     
+    jwt.verify(token, "Jf3@7lP9$Nk2M6vXr8Zw1Bc5#Hd4LqYj", async(err, payload)=>{
+        if(err)res.status(403).json({message: "Token is not Valid!"});
+        if(!payload.isAdmin){
+            return res.status(403).json({message: "Not Autorized!"});
+        }
+    })
+
+    res.status(200).json({message: "You are Authenticated"});
+
 };
 
