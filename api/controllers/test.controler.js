a2304abf996cd7688d973ab91536b7ca23da5ed8
Nimeth Fernando <108478292+nimeth42@users.noreply.github.com>
2024-09-06 11:46:20 +0530

    web token

--- api/controllers/test.controler.js
+++ api/controllers/test.controler.js
@@ -1,9 +1,17 @@
+import jwt from "jsonwebtoken";
+
+
 export const shouldBeLoggedIn = async (req,res)=>{
     const token = req.cookies.token;
 
     if(!token) return res.status(401).json({message: "Not Authenticated!"});
+    
+    jwt.verify(token, "Jf3@7lP9$Nk2M6vXr8Zw1Bc5#Hd4LqYj", async(err, payload)=>{
+        if(err)res.status(403).json({message: "Token is not Valid!"});
+    })
+
 };
 
 export const shouldBeAdmin = async (req,res)=>{
-
+    
 };
